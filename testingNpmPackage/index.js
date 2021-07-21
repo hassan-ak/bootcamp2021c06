f18e2bfbebdfd87289f4b7a3a6299213789cf004
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-07-21 15:07:17 +0500

    Test Published Public Local Package
    Steps: -
          a: - Create new directory “testingNpmPackage”
          b: - “npm init” to initialize a new project
          c: - “npm I -save-dev @types/node” to install node types
          d: - “npm i @hassan-ak/npmtestpackage” to install published local package
          e: - Create .gitignore
          f: - Create index.ts
          g: - compile and execute

---
+++ testingNpmPackage/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var npmtestpackage_1 = require("@hassan-ak/npmtestpackage");
+npmtestpackage_1.hello("Hassan");
---
+++ testingNpmPackage/index.ts
@@ -0,0 +1,3 @@
+import { hello } from "@hassan-ak/npmtestpackage";
+
+hello("Hassan");
